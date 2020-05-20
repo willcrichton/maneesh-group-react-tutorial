9f18790cb71c13c9a1d0411a08a376affde69e1d
Will Crichton <wcrichto@cs.stanford.edu>
2020-05-20 13:54:53 -0700

    Refactor internal components

--- src/index.js
+++ src/index.js
@@ -9,26 +9,34 @@ $('.todo-add').click(() => {
   todo_render();
 });
 
+function todo_delete(i) {
+  let delete_dom = $('<button class="todo-delete">Delete</button>');
+  delete_dom.click(() => {
+    todos.splice(i, 1);
+    todo_render();
+  });
+  return delete_dom;
+}
+
+function todo_input(todo) {
+  let input_dom = $(
+    `<input type="text" placeholder="Empty todo" value="${todo.text}"/>`);
+  input_dom.change(function() {
+    todo.text = $(this).val();
+  });
+  return input_dom;
+}
+
 function todo_render() {
   let all_todo_dom = todos.map((todo, i) => {
-    let todo_dom = $(`<div class="todo">
-    <input type="text" placeholder="Empty todo" value="${todo.text}"/>
-    <button class="todo-delete">Delete</button>
-</div>`);
+    let todo_dom = $(`<div class="todo"></div>`);
 
-    todo_dom.find('.todo-delete').click(() => {
-      todos.splice(i, 1);
-      todo_render();
-    });
-
-    todo_dom.find('input').change(function() {
-      todo.text = $(this).val();
-    });
+    todo_dom.append(todo_input(todo));
+    todo_dom.append(todo_delete(i));
 
     return todo_dom;
   });
 
   $('.todo-list').html(all_todo_dom);
-
-  $('.todo-count').html($('.todo').length);
+  $('.todo-count').html(todos.length);
 }
